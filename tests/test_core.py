d8a36058242d2577cb1f79ce1700d8c0b0c7d5cf
michael-masarik <masarikfamilymichael@gmail.com>
2025-05-15 10:28:11 -0500

    Update test_core.py

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,5 +1,5 @@
 from notion_upload.core import notion_upload
-NOTION_KEY = "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"
+NOTION_KEY = "***"
 test_upload = notion_upload(
     "test.jpg",
     "test.jpg",
