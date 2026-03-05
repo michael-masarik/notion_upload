0d16dc33e32879bb782fb9ee112c41e8ccd4e465
michael-masarik <masarikfamilymichael@gmail.com>
2026-03-04 18:27:44 -0600

    Core logic, single part and external upload created

--- notion_upload/core.py
+++ notion_upload/core.py
@@ -3,9 +3,16 @@ import re
 from typing import BinaryIO
 
 import requests
+import mimetypes
+
+ALLOWED_MIME_TYPES = {}  # TODO
 
 
 class FileToLarge(Exception):
+    pass
+
+
+class InvaildMIME(Exception):
     pass
 
 
@@ -29,6 +36,7 @@ class MultiPartUpload:
 
 
 class notionUpload:
+    NOTION_URL = "https://api.notion.com/v1/file_uploads"
 
     def __init__(
         self,
@@ -37,9 +45,12 @@ class notionUpload:
         api_key: str,
         enforce_max_size=True,
     ):
+
         self.filePath = file_path
         self.fileName = file_name
+        self.mimeType = mimetypes.guess_type(file_path)[0] or "application/octet-stream"
         self.apiKey = api_key
+        self.bulkUpload = MultiPartUpload(self.apiKey)
         if enforce_max_size:
             self.maxBytes = 5242880
         else:
@@ -58,8 +69,74 @@ class notionUpload:
                     self.multiPart = True
             else:
                 raise FileNotFoundError(self.filePath)
+        if self.mimeType not in ALLOWED_MIME_TYPES:
+            raise InvaildMIME(f"{self.mimeType} is not a support MIME type")
+        if mimetypes.guess_type(self.fileName)[0] != self.mimeType:
+            raise InvaildMIME("File extension and MIME type do not match")
 
-        # self.mime_type = mimetypes.guess_type(file_path)[0] or 'application/octet-stream'
+    def initiate_upload(self, external=False):
+        payload = {"filename": self.fileName, "content_type": self.mimeType}
+        if external:
+            payload["mode"] = "external_url"
+            payload["external_url"] = self.filePath
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "Authorization": f"Bearer {self.apiKey}",
+            "Notion-Version": "2025-09-03",
+        }
+
+        try:
+            response = requests.post(
+                url=self.NOTION_URL, json=payload, headers=headers, timeout=10
+            )
+            if response.status_code == 200:
+                file_id = response.json().get("id")
+                if external == False:
+                    print("🚀 Upload successfully started! File ID: " + file_id)
+                else:
+                    print("✅ Upload successful! File ID: " + file_id)
+                return file_id
+            else:
+                print("❌ Upload failed:", response.status_code, response.text)
+                return None
+        except requests.RequestException as e:
+            print("🌐 Upload failed due to a network error:", e)
+            return None
+
+    def singleUpload(self):
+        file_id = self.initiate_upload()
+        if file_id is None:
+            return None
+
+        try:
+            with open(self.filePath, "rb") as f:
+                files = {"file": (self.fileName, f, self.mimeType)}
+
+                upload_url = f"https://api.notion.com/v1/file_uploads/{file_id}/send"
+                headers = {
+                    "Authorization": f"Bearer {self.apiKey}",
+                    "Notion-Version": "2022-06-28",
+                }
+
+                response = requests.post(
+                    upload_url, headers=headers, files=files, timeout=10
+                )
+
+                if response.status_code == 200:
+                    print("✅ Upload successful! File ID: " + file_id)
+                else:
+                    print(
+                        "📤 Upload failed at file send stage:",
+                        response.status_code,
+                        response.text,
+                    )
+        except FileNotFoundError:
+            print(f"📁 File not found: {self.filePath}")
+        return file_id
+
+    def upload(self):
+        pass
 
 
 # Type alias
